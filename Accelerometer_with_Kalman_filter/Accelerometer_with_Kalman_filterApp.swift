2615acce5ff54e1a0eaecd45935c30a2e216f4a3
Marek Forys <Marek.Forys@tomtom.com>
2021-08-09 20:59:42 +0200

    Skeleton of iOS sample app "Hello world"

---
+++ Accelerometer_with_Kalman_filter/Accelerometer_with_Kalman_filterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accelerometer_with_Kalman_filterApp.swift
+//  Accelerometer_with_Kalman_filter
+//
+//  Created by Marek Forys on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Accelerometer_with_Kalman_filterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accelerometer_with_Kalman_filter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accelerometer_with_Kalman_filter
+//
+//  Created by Marek Forys on 09/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
