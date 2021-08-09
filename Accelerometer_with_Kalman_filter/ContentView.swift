2e189c8e2b4659148eee572844915959159e1af0
Marek Forys <Marek.Forys@tomtom.com>
2021-08-09 21:07:38 +0200

    ContentView.swift filled with the basic implementation

--- Accelerometer_with_Kalman_filter/ContentView.swift
+++ Accelerometer_with_Kalman_filter/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject var dataController = DataController()
+    @State var useKalmanFilter = false
+
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+        Text("MF Accelerator Kalman")
             .padding()
+    
+            Group {
+                Text("Acceleration").bold()
+                Text("X = \(dataController.accelerationX)")
+                Text("Y = \(dataController.accelerationY)")
+                Text("Z = \(dataController.accelerationZ)")
+            }
+
+            Group {
+                Text("Gyroscope").bold()
+                Text("X = \(dataController.rotationRateX)")
+                Text("Y = \(dataController.rotationRateY)")
+                Text("Z = \(dataController.rotationRateZ)")
+            }
+
+            Spacer()
+
+            Circle()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .offset(x: CGFloat(dataController.accelerationX) * 200, y: CGFloat(dataController.accelerationY) * 200)
+                .padding()
+
+            Spacer()
+
+            Button{
+                useKalmanFilter.toggle()
+            }
+            label: {
+                Text(useKalmanFilter ? "Kalman Filter ON" : "Kalman Filter OFF")
+                    .bold()
+                    .padding(20)
+            }
+            .contentShape(Rectangle())
+            .padding()
+            .background(Color(red: 0.1, green: 0.1, blue: 0))
+            .clipShape(Capsule())
+        }
     }
 }
 
